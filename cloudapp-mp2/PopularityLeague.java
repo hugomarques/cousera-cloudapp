4c527b9fc013e2a28be4f0513ffe60e86093d198
marquesh <marques@amazon.com>
2015-09-14 18:57:39 -0700

    Partial popularity league

--- cloudapp-mp2/PopularityLeague.java
+++ cloudapp-mp2/PopularityLeague.java
@@ -1,26 +1,19 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class PopularityLeague extends Configured implements Tool {
 
@@ -31,8 +24,74 @@ public class PopularityLeague extends Configured implements Tool {
 
     @Override
     public int run(String[] args) throws Exception {
-        // TODO
+        Configuration conf = this.getConf();
+        FileSystem fs = FileSystem.get(conf);
+        Path tmpPath = new Path("/mp2/tmp");
+        fs.delete(tmpPath, true);
+
+        Job linkCountJob = Job.getInstance(conf, "Link Count");
+
+        linkCountJob.setOutputKeyClass(IntWritable.class);
+        linkCountJob.setOutputValueClass(IntWritable.class);
+
+        linkCountJob.setMapperClass(LinkCountMap.class);
+        linkCountJob.setReducerClass(LinkCountReduce.class);
+
+        FileInputFormat.setInputPaths(linkCountJob, new Path(args[1]));
+        FileOutputFormat.setOutputPath(linkCountJob, tmpPath);
+
+        linkCountJob.setJarByClass(PopularityLeague.class);
+
+        linkCountJob.waitForCompletion(true);
+
+        return 1;
     }
 
-    // TODO
+    public static class LinkCountMap extends Mapper<Object, Text, IntWritable, IntWritable> {
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            final StringTokenizer stringTokenizer = new StringTokenizer(value.toString());
+            while (stringTokenizer.hasMoreTokens()) {
+                String link = stringTokenizer.nextToken().trim();
+                if (!link.endsWith(":")) {
+                    context.write(new IntWritable(Integer.valueOf(link)), new IntWritable(1));
+                }
+            }
+        }
+    }
+
+    public static class LinkCountReduce extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+        @Override
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val: values) {
+                sum += val.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
+
+    public static class PopularLeagueMap extends Mapper<Object, Text, IntWritable, IntWritable> {
+        @Override
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            final StringTokenizer stringTokenizer = new StringTokenizer(value.toString());
+            while (stringTokenizer.hasMoreTokens()) {
+                String link = stringTokenizer.nextToken().trim();
+                if (!link.endsWith(":")) {
+                    context.write(new IntWritable(Integer.valueOf(link)), new IntWritable(1));
+                }
+            }
+        }
+    }
+
+    public static class PopularLeagueReduce extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+        @Override
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val: values) {
+                sum += val.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
 }
