883d5684d5e35fdef39a05ef4860c2a0fc593a7f
marquesh <marques@amazon.com>
2015-09-27 11:33:14 -0700

    Adding cloudmp3 to git tree

---
+++ cloudapp-mp3/src/TopWordFinderTopologyPartB.java
@@ -0,0 +1,51 @@
+
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
+
+/**
+ * This topology reads a file and counts the words in that file
+ */
+public class TopWordFinderTopologyPartB {
+
+  public static void main(String[] args) throws Exception {
+
+
+    TopologyBuilder builder = new TopologyBuilder();
+
+    Config config = new Config();
+    config.setDebug(true);
+
+
+    /*
+    ----------------------TODO-----------------------
+    Task: wire up the topology
+
+    NOTE:make sure when connecting components together, using the functions setBolt(name,…) and setSpout(name,…),
+    you use the following names for each component:
+    FileReaderSpout -> "spout"
+    SplitSentenceBolt -> "split"
+    WordCountBolt -> "count"
+
+
+
+    ------------------------------------------------- */
+    builder.setSpout("spout", new FileReaderSpout(args[0]), 1);
+
+    builder.setBolt("split", new SplitSentenceBolt(), 8).shuffleGrouping("spout");
+    builder.setBolt("count", new WordCountBolt(), 12).fieldsGrouping("split", new Fields("word"));
+
+
+
+    config.setMaxTaskParallelism(3);
+
+    LocalCluster cluster = new LocalCluster();
+    cluster.submitTopology("word-count", config, builder.createTopology());
+
+    //wait for 2 minutes and then kill the job
+    Thread.sleep( 2 * 60 * 1000);
+
+    cluster.shutdown();
+  }
+}
